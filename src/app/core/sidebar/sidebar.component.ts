5181522087326a2dc9b12c4b8352b990a4c24196
jaimin-robiotic <j.panchal@robiotic.com>
2023-12-14 15:47:47 +0530

    Remove API Key and implement changes.

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -3,11 +3,13 @@ import { StoreService } from '../../services/store.service';
 import { CommonModule } from '@angular/common';
 import { pipe, take } from 'rxjs';
 import { listImage, tagList } from '../../layout/layout.model';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { SkeletonLoaderComponent } from '../../shared/skeleton-loader/skeleton-loader.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SkeletonLoaderComponent],
   providers: [StoreService],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
@@ -15,45 +17,66 @@ import { listImage, tagList } from '../../layout/layout.model';
 export class SidebarComponent implements OnInit {
   tagsArray: tagList[] = [];
   private storeService = inject(StoreService);
-  selectedTag:any;
+  selectedTag: any;
   @Output() tagName: EventEmitter<string> = new EventEmitter();
-  constructor() {
-    
-  }
+  isLoading = false;
+  skeletonData = {
+    count: '16',
+    appearance: 'line',
+    animation: 'pulse',
+    theme: {
+      width: '100px',
+      height: '20px',
+      'margin-right': '10px',
+      'border-radius': '10px',
+      background:
+        'linear-gradient(90deg, #D9D9D9 0%, rgba(217, 217, 217, 0) 100%)',
+    },
+  };
+  constructor() {}
   ngOnInit(): void {
-    this.storeService.getGalleryList().subscribe({
-      next:(result) => {
-      this.tagsArray = [];
-      if(result.length> 0)
-      result.forEach((item:listImage) => {
-        item.tags.forEach((e:string)=>{
-          if (
-            !this.tagsArray.some(
-              (existingItem:tagList) => existingItem.tagName === e
-            )
-          ) {
-            this.tagsArray.push(this.selectedTag && this.selectedTag.tagName ==  e ? { tagName: e, isActive: true } :{ tagName: e, isActive: false });
-          }
-        })
+    this.isLoading = true;
+    this.storeService.getTagList().subscribe({
+      next: (result) => {
         
-      });
-    }});
+        this.tagsArray = [];
+        if (result.length > 0){
+          result.forEach((item: listImage) => {
+            item.tags.forEach((e: string) => {
+              if (
+                !this.tagsArray.some(
+                  (existingItem: tagList) => existingItem.tagName === e
+                )
+              ) {
+                this.tagsArray.push(
+                  this.selectedTag && this.selectedTag.tagName == e
+                    ? { tagName: e, isActive: true }
+                    : { tagName: e, isActive: false }
+                );
+              }
+            });
+          });
+        }
+        this.isLoading = false;
+      },
+    });
+   
   }
 
   onTagFilter(tagData: tagList) {
-    this.tagsArray.forEach(item => {
+    this.tagsArray.forEach((item) => {
       if (item.tagName !== tagData.tagName) {
         item.isActive = false;
       }
     });
     tagData.isActive = !tagData.isActive;
     this.selectedTag = tagData;
-    this.tagName.emit(tagData.isActive ? tagData.tagName: '');
+    this.tagName.emit(tagData.isActive ? tagData.tagName : '');
   }
 
-  onResetFilter(){
-    this.selectedTag = null
-    this.tagsArray.forEach(item => item.isActive = false);
-    this.tagName.emit('')
+  onResetFilter() {
+    this.selectedTag = null;
+    this.tagsArray.forEach((item) => (item.isActive = false));
+    this.tagName.emit('');
   }
 }
--- src/app/layout/image-list/image-list.component.ts
+++ src/app/layout/image-list/image-list.component.ts
@@ -14,10 +14,10 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { AppConst } from '../../helper/app-const';
 import { SharedService } from '../../services/shared.service';
 import { listImage, viewImageData } from '../layout.model';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { take } from 'rxjs';
 import { ImageCardComponent } from '../../shared/image-card/image-card.component';
 import { UploadImageDialogComponent } from '../../shared/upload-image-dialog/upload-image-dialog.component';
+import { SkeletonLoaderComponent } from '../../shared/skeleton-loader/skeleton-loader.component';
 
 @Component({
   selector: 'app-image-list',
@@ -28,13 +28,13 @@ import { UploadImageDialogComponent } from '../../shared/upload-image-dialog/upl
     MatButtonModule,
     InfiniteScrollModule,
     UploadImageDialogComponent,
-    NgxSkeletonLoaderModule,
+    SkeletonLoaderComponent,
     ImageCardComponent,
   ],
   templateUrl: './image-list.component.html',
   styleUrl: './image-list.component.scss',
 })
-export class ImageListComponent implements OnChanges {
+export class ImageListComponent implements OnInit, OnChanges {
   imageList: listImage[] = [];
   @Input() searchData: string = '';
   @Input() tagData: string = '';
@@ -43,12 +43,45 @@ export class ImageListComponent implements OnChanges {
   private storeService = inject(StoreService);
   private sharedService = inject(SharedService);
   private dialog = inject(MatDialog);
-  isLoding = false;
-
+  isLoading = false;
+  isEmpty!:boolean
+  skeletonData = {
+    count: '16',
+    appearance: 'circle',
+    animation: 'pulse',
+    theme: {
+      width: '150px',
+      height: '200px',
+      'margin-right': '10px',
+      'border-radius': '10px',
+      background:
+        'linear-gradient(90deg, #D9D9D9 0%, rgba(217, 217, 217, 0) 100%)',
+    },
+  };
+  ngOnInit(): void {
+    this.isLoading =true
+    this.storeService.getTagList().subscribe({
+      next: (res) => {
+        this.resetImageArray();
+        if (this.searchData == '' && this.tagData == '') {
+          this.getPagiNation();
+        }
+        if (this.searchData || this.tagData) {
+          console.log('object');
+          this.loadItems();
+        }
+      },
+    });
+  }
   ngOnChanges(changes: SimpleChanges): void {
     console.log('onchnage', changes);
     this.resetImageArray();
-    if (this.searchData == '' && this.tagData == '') {
+    if (
+      this.searchData == '' &&
+      !changes['searchData']?.firstChange &&
+      this.tagData == '' &&
+      !changes['tagData']?.firstChange
+    ) {
       this.getPagiNation();
     }
     if (this.searchData || this.tagData) {
@@ -63,67 +96,80 @@ export class ImageListComponent implements OnChanges {
 
   loadItems() {
     console.log(this.searchData, this.tagData);
-    this.isLoding = true;
+    this.isLoading = true;
     this.storeService
       .getFilteredData(this.searchData, this.tagData)
       .pipe(take(1))
       .subscribe({
         next: (res) => {
-          this.isLoding = false;
-          console.log(res);
+          this.isLoading = false;
+          
           if (res.length > 0) {
-            console.log(res);
+            this.isEmpty = false
+            
             this.lastVisible = res[res.length - 1];
             this.imageList = res;
+          }else{
+            this.isEmpty = true
           }
+          console.log(this.isEmpty);
         },
       });
   }
 
   getPagiNation() {
     console.log(this.lastVisible);
-    this.isLoding = true;
+    this.isLoading = true;
     this.storeService
       .getPaginatedData(this.lastVisible)
       .pipe(take(1))
       .subscribe({
         next: (res: any) => {
-          this.isLoding = false;
-          console.log(res);
+          this.isLoading = false;
+          
           if (res.length > 0) {
-            console.log(res);
+            this.isEmpty = false
+            
             this.lastVisible = res[res.length - 1];
             this.imageList = this.imageList.concat(res);
+          }else{
+            if(this.imageList.length >0){
+              this.isEmpty = false
+            }else{
+              this.isEmpty = true
+            }
           }
+          console.log(this.isEmpty);
         },
       });
   }
   //upload image dialog
   uploadImageDialog() {
-    const dialogRef = this.dialog.open(UploadImageDialogComponent);
+    const dialogRef = this.dialog.open(UploadImageDialogComponent,{
+      autoFocus:false,
+    });
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
       if (result == 'yes') {
-        this.resetImageArray();
-        this.loadItems();
         this.sharedService.loggerSuccess('File uploaded successfully.');
       }
     });
   }
 
-  viewDialog(e:viewImageData){
+  viewDialog(e: viewImageData) {
     console.log(e);
-    if(e.type == 'view'){
-      const dialogRef = this.dialog.open(UploadImageDialogComponent,{
-        data: e
+    if (e.type == 'view') {
+      const dialogRef = this.dialog.open(UploadImageDialogComponent, {
+        autoFocus:false,
+        data: e,
       });
 
-    dialogRef.afterClosed().subscribe((result) => {
-      console.log(`Dialog result: ${result}`);
-      if (result == 'yes') {
-      }
-    });
+      dialogRef.afterClosed().subscribe((result) => {
+        console.log(`Dialog result: ${result}`);
+        if (result == 'yes') {
+        }
+      });
     }
   }
 
